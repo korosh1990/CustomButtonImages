11629a7be6326c49d67dc042e8746fb130b1c728
Korosh1990 <saberikorosh@gmail.com>
2018-07-07 17:34:16 -0700

    Initial commit

---
+++ app/src/main/java/ir/androidlife/custombuttonimages/MainActivity.java
@@ -0,0 +1,39 @@
+package ir.androidlife.custombuttonimages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button customButton = findViewById(R.id.custom_button);
+        Switch switchEnableButton = findViewById(R.id.switch_enable_button);
+
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "اندروید لایف", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        switchEnableButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    customButton.setEnabled(true);
+                } else {
+                    customButton.setEnabled(false);
+                }
+            }
+        });
+    }
+}
